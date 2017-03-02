ad05a2ec30915eacee0b7b1da3057f85fe102b18
Steingrímur <steingrimur@Steingrimurs-MacBook-Pro.local>
2017-03-02 22:06:50 +0000

    Some changes

--- single.php
+++ single.php
@@ -1,18 +1,49 @@
 <?php include('header.php'); ?>
 <div id="single-page">
   <div class="post-content">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <!-- If post 28 (Veitingar) is present display list of available items. Else display standard Veitingar post. -->
+    <?php if(is_single('28')) {
+      $the_query = new WP_Query( array(
+        'post_type' => 'veitingahus'
+      ));
 
-        <h1>
-          <?php the_title(); ?>
-        </h1>
+      if ( $the_query->have_posts() ) {
+        echo '<ul>';
+        while ( $the_query->have_posts() ) {
+          $the_query->the_post();
+            echo '<li>';
+              echo '<a href="';
+                echo get_permalink();
+              echo '">';
+                the_post_thumbnail();
+                // echo '<div>';
+                //   echo '<h1>';
+                //     echo get_the_title();
+                //   echo '</h1>';
+                //   echo '<div>';
+                //     echo the_excerpt(); //Restaurant logo
+                //   echo '</div>';
+                // echo '</div>';
+              echo '</a>';
+            echo '</li>';
+        }
+        echo '</ul>';
+        /* Restore original Post Data */
+        wp_reset_postdata();
+      } else {
+        // no posts found
+      }
+    } else {
+      if ( have_posts() ) : while ( have_posts() ) : the_post();
+        echo '<h1>';
+          the_title();
+        echo '</h1>';
+          the_content();
 
-        <?php the_content(); ?>
-
-        <?php include('google-map.php'); ?>
-
-
-    <?php endwhile; endif; ?>
+        include('google-map.php');
+        endwhile; endif;
+      }
+    ?>
   </div>
 </div>
 
