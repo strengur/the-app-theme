db082c024c7a711f0054ebb2354aa8334cd7a2e2
Steingrímur <steingrimur@Steingrimurs-MacBook-Pro.local>
2017-03-04 07:50:46 +0000

    Some

--- single.php
+++ single.php
@@ -1,49 +1,51 @@
 <?php include('header.php'); ?>
 <div id="single-page">
   <div class="post-content">
-    <!-- If post 28 (Veitingar) is present display list of available items. Else display standard Veitingar post. -->
-    <?php if(is_single('28')) {
-      $the_query = new WP_Query( array(
-        'post_type' => 'veitingahus'
-      ));
+    <div class="veitingar-listings">
+      <!-- If post 28 (Veitingar) is present display list of available items. Else display standard Veitingar post. -->
+      <?php if(is_single('28')) {
+        $the_query = new WP_Query( array(
+          'post_type' => 'veitingahus'
+        ));
 
-      if ( $the_query->have_posts() ) {
-        echo '<ul>';
-        while ( $the_query->have_posts() ) {
-          $the_query->the_post();
-            echo '<li>';
-              echo '<a href="';
-                echo get_permalink();
-              echo '">';
-                the_post_thumbnail();
-                // echo '<div>';
-                //   echo '<h1>';
-                //     echo get_the_title();
-                //   echo '</h1>';
-                //   echo '<div>';
-                //     echo the_excerpt(); //Restaurant logo
-                //   echo '</div>';
-                // echo '</div>';
-              echo '</a>';
-            echo '</li>';
+        if ( $the_query->have_posts() ) {
+          echo '<ul>';
+          while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+              echo '<li>';
+                echo '<a href="';
+                  echo get_permalink();
+                echo '">';
+                  the_post_thumbnail();
+                  // echo '<div>';
+                  //   echo '<h1>';
+                  //     echo get_the_title();
+                  //   echo '</h1>';
+                  //   echo '<div>';
+                  //     echo the_excerpt(); //Restaurant logo
+                  //   echo '</div>';
+                  // echo '</div>';
+                echo '</a>';
+              echo '</li>';
+          }
+          echo '</ul>';
+          /* Restore original Post Data */
+          wp_reset_postdata();
+        } else {
+          // no posts found
         }
-        echo '</ul>';
-        /* Restore original Post Data */
-        wp_reset_postdata();
       } else {
-        // no posts found
-      }
-    } else {
-      if ( have_posts() ) : while ( have_posts() ) : the_post();
-        echo '<h1>';
-          the_title();
-        echo '</h1>';
-          the_content();
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+          echo '<h1>';
+            the_title();
+          echo '</h1>';
+            the_content();
 
-        include('google-map.php');
-        endwhile; endif;
-      }
-    ?>
+          include('google-map.php');
+          endwhile; endif;
+        }
+      ?>
+    </div>
   </div>
 </div>
 
