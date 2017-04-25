d59760e0160773bcae0179dd66b97c8decd5c2eb
Steingrímur <steingrimur@740.is>
2017-04-25 08:23:40 +0000

    Few changes

--- single.php
+++ single.php
@@ -54,7 +54,9 @@
           echo '<h4>';
             wp_title( $sep = '');
           echo '</h4>';
-          echo the_content();
+          echo '<p>Liðin má sjá neðst á síðunni ásamt hver er liðstjóri - Góða skemmtun og farið varlega!';
+            echo the_content();
+          echo '</p>';
           echo '<ul>';
           while ( $the_query->have_posts() ) {
             $the_query->the_post();
@@ -78,7 +80,90 @@
                 echo '</p>';
               echo '</li>';
           }
-          echo '</ul>';
+          echo '</ul>'; ?>
+          <div class="teams">
+            <h5>Lið 1</h5>
+            <ul class="">
+             	<li>Jói Helga (Liðsstjóri)</li>
+             	<li>Atli</li>
+             	<li>Björn Þorkels</li>
+             	<li>Einar</li>
+             	<li>Biggi</li>
+            </ul>
+            <h5>Lið 2</h5>
+            <ul>
+             	<li>Ingi Þór (Liðsstjóri)</li>
+             	<li>Björn</li>
+             	<li>Helgi</li>
+             	<li>Tómas</li>
+             	<li>Árni</li>
+             	<li>Steingrímur</li>
+            </ul>
+            <h5>Lið 3</h5>
+            <ul>
+             	<li>Pétur (Liðsstjóri)</li>
+             	<li>Lilja</li>
+             	<li>Sigurrós</li>
+             	<li>Svanhvít</li>
+             	<li>Bjarni</li>
+            </ul>
+            <h5>Lið 4</h5>
+            <ul>
+             	<li>Hulda (Liðsstjóri)</li>
+             	<li>Sindri</li>
+             	<li>Teddi</li>
+             	<li>Sonja</li>
+             	<li>Trigger</li>
+            </ul>
+            <h5>Lið 5</h5>
+            <ul>
+             	<li>Maddi (Liðsstjóri)</li>
+             	<li>Ívar</li>
+             	<li>Jói Más</li>
+             	<li>Stefán H</li>
+             	<li>Bragi</li>
+            </ul>
+            <h5>Lið 6</h5>
+            <ul>
+             	<li>Hjalti (Liðsstjóri)</li>
+             	<li>Jón Bjarni</li>
+             	<li>Geirlaugur</li>
+             	<li>Brynjar Gústafs</li>
+             	<li>Robbi</li>
+            </ul>
+            <h5>Lið 7</h5>
+            <ul>
+             	<li>Indriði (Liðsstjóri)</li>
+             	<li>Örn</li>
+             	<li>Stefán Jó</li>
+             	<li>Hjálmur</li>
+             	<li>Daði</li>
+            </ul>
+            <h5>Lið 8</h5>
+            <ul>
+             	<li>Unnar (Liðsstjóri)</li>
+             	<li>Tryggvi G</li>
+             	<li>Benedikt</li>
+             	<li>Hanna</li>
+            </ul>
+            <h5>Lið 9</h5>
+            <ul>
+             	<li>Izaar (Liðsstjóri)</li>
+             	<li>Sverrir</li>
+             	<li>Heiðar</li>
+             	<li>Kristinn Geir</li>
+             	<li>Guðmundur</li>
+            </ul>
+            <h5>Lið 10</h5>
+            <ul>
+             	<li>Jón Kristinn (Liðsstjóri)</li>
+             	<li>Sóli</li>
+             	<li>Magnús</li>
+             	<li>Kristján</li>
+             	<li>Ármann</li>
+            </ul>
+          </div>
+          <?php
           echo '</div>';
           /* Restore original Post Data */
           wp_reset_postdata();
